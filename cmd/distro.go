f20e9cadc12eb51be5ce74701598cee8ff2e47f9
Paulo Neves <paulo.zagalo.neves@gmail.com>
2025-12-27 22:29:09 +0000

    fix get distro templates

--- cmd/distro.go
+++ cmd/distro.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"os/user"
 	"path/filepath"
 
 	"gopkg.in/ini.v1"
@@ -14,9 +16,27 @@ type DistroInfo struct {
 	OSVariant string
 }
 
+// Função auxiliar para procurar template
+func findTemplate(filename string) (string, error) {
+	// 1. Procurar na pasta local templates/
+	local := filepath.Join("templates", filename)
+	if _, err := os.Stat(local); err == nil {
+		return local, nil
+	}
+	// 2. Procurar em ~/.config/kvm-compose/templates/
+	usr, _ := user.Current()
+	if usr != nil {
+		home := filepath.Join(usr.HomeDir, ".config", "kvm-compose", "templates", filename)
+		if _, err := os.Stat(home); err == nil {
+			return home, nil
+		}
+	}
+	return "", fmt.Errorf("template %s não encontrado", filename)
+}
+
 // loadDistroInfo lê o ficheiro templates/<distro>.ini e retorna as informações da distro
 func loadDistroInfo(distro string) (*DistroInfo, error) {
-	iniPath := filepath.Join("templates", fmt.Sprintf("%s.ini", distro))
+	iniPath, err := findTemplate(fmt.Sprintf("%s.ini", distro)) //filepath.Join("templates", fmt.Sprintf("%s.ini", distro))
 	cfg, err := ini.Load(iniPath)
 	if err != nil {
 		return nil, fmt.Errorf("erro ao carregar INI da distro: %v", err)
